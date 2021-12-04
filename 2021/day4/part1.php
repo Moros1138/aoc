9c73ee115f39034f28c784ebe55204dc95cefcb2
Moros1138 <moros1138@gmail.com>
2021-12-04 12:57:54 -0500

    clear day 4, was copied from day 3 as template

--- 2021/day4/part1.php
+++ 2021/day4/part1.php
@@ -1,57 +1,7 @@
 <?php
 
+// BEGIN PARSE INPUT
 if(@$_SERVER['argv'][1] == "sample")
     $input = file("sample.txt");
 else
     $input = file("real.txt");
-
-for($i = 0; $i < count($input); $i++)
-    $input[$i] = trim($input[$i]);
-
-
-$bits = strlen($input[0]);
-
-// gamma
-
-function most_common($index)
-{
-    global $input;
-    $c1 = 0;
-    $c2 = 0;
-    foreach($input as $i)
-    {
-        $c2++;
-        if($i[$index] == "0")
-            $c1++;
-    }
-    
-    $c2 /= 2;
-    
-    if($c2 > $c1)
-        return 0;
-    
-    return 1;
-}
-
-function least_common($index)
-{
-    $a = most_common($index);
-    return ($a == 0) ? 1 : 0;
-}
-
-$epsilon = "";
-$gamma = "";
-
-for($i = 0; $i < $bits; $i++)
-    $gamma .= least_common($i);
-
-
-for($i = 0; $i < $bits; $i++)
-    $epsilon .= most_common($i);
-
-$gamma = bindec($gamma);
-$epsilon = bindec($epsilon);
-
-echo $gamma . "\n";
-echo $epsilon . "\n";
-echo ($gamma * $epsilon) . "\n";
